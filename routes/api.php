5be445c95c5abd292b0bcc52639c652e55741f67
PutriDeylin <putrideylin82@gmail.com>
2023-11-26 15:51:08 +0700

    [CREATE] penambahan file/komponen tugas 29 api

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\ProductApiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+// });
+
+Route::get('/products', [ProductApiController::class, 'index']);
+Route::get('/products/{id}', [ProductApiController::class, 'show']);
+Route::post('/products', [ProductApiController::class, 'store']);
+Route::put('/products/{id}', [ProductApiController::class, 'update']);
+Route::delete('/products/{id}', [ProductApiController::class, 'destroy']);
+
